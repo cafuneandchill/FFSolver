592ebd83a55654762bd3c0a14ac0301dcd9870ec
cafuneandchill <43551330+cafuneandchill@users.noreply.github.com>
2025-02-28 10:48:13 +0300

    I forgot we're doing tarot cards lmao

--- cards.py
+++ cards.py
@@ -2,18 +2,34 @@ from enum import Enum
 
 
 class Suit(Enum):
-    SPADES = 0
-    HEARTS = 1
-    DIAMONDS = 2
-    CLUBS = 3
+    SWORDS = 0
+    STAVES = 1
+    CUPS = 2
+    COINS = 3
 
 
-class Card:
-    def __init__(self, suit: Suit, rank: int):
+class SuitColor(Enum):
+    RED = 0
+    BLACK = 1
+
+
+class MinorArcanaCard:
+    def __init__(self, rank: int, suit: Suit):
         """
 
-        :param suit: Card suit
-        :param rank: Card rank; `A` = 1, `J` = 11, `Q` = 12, `K` = 13
+        :param rank: Card rank, ranging from 1 to 13; `A` = 1, `J` = 11, `Q` = 12, `K` = 13
+        :param suit: Card suit (swords, staves, cups, coins)
         """
+        if rank not in range(1, 13):
+            raise ValueError("Rank out of range")
+
+        self.rank = rank
         self.suit = suit
+
+
+class MajorArcanaCard:
+    def __init__(self, rank: int):
+        if rank not in range(0, 21):
+            raise ValueError("Rank out of range")
+
         self.rank = rank
